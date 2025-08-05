92cd491b842799418b553aaada5a3f47dcbf7076
moatasim <mo32000a@gmail.com>
2025-08-05 15:59:09 +0300

    added products initial products test

--- src/components/Products/ProductsGrid/allProducts.ts
+++ src/components/Products/ProductsGrid/allProducts.ts
@@ -1,29 +1,29 @@
 import { StaticImageData } from "next/image"
 
-import image1 from "../../../../public/imgs/products/1.jpeg";
-// import image2 from "../../../../public/imgs/products/2.jpeg";
-// import image3 from "../../../../public/imgs/products/3.jpeg";
-// import image4 from "../../../../public/imgs/products/4.jpeg";
-import image5 from "../../../../public/imgs/products/5.jpeg";
-// import image6 from "../../../../public/imgs/products/6.jpeg";
-import image7 from "../../../../public/imgs/products/7.jpeg";
-// import image8 from "../../../../public/imgs/products/8.jpeg";
-import image9 from "../../../../public/imgs/products/9.jpeg";
-// import image10 from "../../../../public/imgs/products/10.jpeg";
-import image11 from "../../../../public/imgs/products/11.png";
-// import image12 from "../../../../public/imgs/products/12.png";
-// import image13 from "../../../../public/imgs/products/13.jpeg";
-// import image14 from "../../../../public/imgs/products/14.jpeg";
-import image15 from "../../../../public/imgs/products/15.jpeg";
-// import image16 from "../../../../public/imgs/products/16.jpeg";
-// import image17 from "../../../../public/imgs/products/17.jpeg";
-import image18 from "../../../../public/imgs/products/18.png";
-import image19 from "../../../../public/imgs/products/19.png";
-import image20 from "../../../../public/imgs/products/20.png";
-// import image21 from "../../../../public/imgs/products/21.jpeg";
-import image22 from "../../../../public/imgs/products/22.png";
-import image23 from "../../../../public/imgs/products/23.jpeg";
-import image24 from "../../../../public/imgs/products/24.jpeg";
+import image1 from "../../../../public/imgs/product-test.png";
+// import image2 from "../../../../public/imgs/product-test.png";
+// import image3 from "../../../../public/imgs/product-test.png";
+// import image4 from "../../../../public/imgs/product-test.png";
+import image5 from "../../../../public/imgs/product-test.png";
+// import image6 from "../../../../public/imgs/product-test.png";
+import image7 from "../../../../public/imgs/product-test.png";
+// import image8 from "../../../../public/imgs/product-test.png";
+import image9 from "../../../../public/imgs/product-test.png";
+// import image10 from "../../../../public/imgs/product-test.png";
+import image11 from "../../../../public/imgs/product-test.png";
+// import image12 from "../../../../public/imgs/product-test.png";
+// import image13 from "../../../../public/imgs/product-test.png";
+// import image14 from "../../../../public/imgs/product-test.png";
+import image15 from "../../../../public/imgs/product-test.png";
+// import image16 from "../../../../public/imgs/product-test.png";
+// import image17 from "../../../../public/imgs/product-test.png";
+import image18 from "../../../../public/imgs/product-test.png";
+import image19 from "../../../../public/imgs/product-test.png";
+import image20 from "../../../../public/imgs/product-test.png";
+// import image21 from "../../../../public/imgs/product-test.png";
+import image22 from "../../../../public/imgs/product-test.png";
+import image23 from "../../../../public/imgs/product-test.png";
+import image24 from "../../../../public/imgs/product-test.png";
 
 
 
